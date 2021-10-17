2b12cb8b4a836f0fd5b0c0fc03f16be11bb3369f
flo-314 <julianflo1916@gmail.com>
2021-10-17 08:56:00 -0300

    contact hecho

--- src/index.js
+++ src/index.js
@@ -40,37 +40,3 @@ contactBtn.addEventListener("click", () => {
   }
 }); 
  
-/* </div>
-
-    <header id="header">
-    <div id="title-container"><h1>La Turba</h1></div>  
-    <p>Av. Varela 503</p>  
-    <nav>
-        <button>Home</button>
-        <button>Menu</button>
-        <button>Contacto</button>
-      </nav>
-    </header>
-    <img id="header-img" src="" alt="">  
-      <main>
-        <div id="title">Nuestro Menu</div>
-        <div id="menu">
-          <div class="menuContainer" id="hamburguesa">  Hamburguesa con Papas Fritas</div>
-          <div class="menuContainer" id="milanesa">Milanesa con Papas Fritas</div>
-          <div class="menuContainer" id="tortilla">Tortilla a la Española</div>
-          <div class="menuContainer" id="muzzarela">Pizza de Muzzarela</div>
-          <div class="menuContainer" id="empanadas">Empanadas de Carne</div>
-          <div class="menuContainer" id="cafe">Cafe con Medialunas</div>
-          <div class="menuContainer" id="cafe">Cafe con Medialunas</div>
-          <div class="menuContainer" id="cafe">Cafe con Medialunas</div>
-          <div class="menuContainer" id="cafe">Cafe con Medialunas</div>
-          <div class="menuContainer" id="cafe">Cafe con Medialunas</div>
-          <div class="menuContainer" id="cafe">Cafe con Medialunas</div>
-          <div class="menuContainer" id="cafe">Cafe con Medialunas</div>
-
-
-      </main>
-    
-    <footer>
-      Page created by flo   <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="">
-    </footer> */
